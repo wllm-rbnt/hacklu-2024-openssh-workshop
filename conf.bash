c18fab6525681d2cd95a59551766376919a810f0
William Robinet <william.robinet@conostix.com>
2024-10-22 09:44:18 +0200

    Initial import

---
+++ conf.bash
@@ -0,0 +1,15 @@
+conf_name="Hack.lu"
+conf_location="Luxembourg"
+conf_year="2024"
+
+ws_title="Exploring OpenSSH: Hands-On Workshop for Beginners"
+ws_author="William Robinet (Conostix S.A.)"
+ws_date="2024-10-22"
+
+document_name="hacklu-2024-openssh-workshop"
+
+gh_repo="https://github.com/wllm-rbnt/hacklu-2024-openssh-workshop"
+gh_repo_short="https://t.ly/QrHP4"
+
+matrix_room="https://matrix.to/#/#Hack.lu_2024-OpenSSH_Workshop:matrix.org"
+matrix_room_short="https://t.ly/wjSQU"
